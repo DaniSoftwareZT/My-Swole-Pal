6a0f6f570db54e5c458f5eb01aa22f22bc2ed3f7
Edwin <emadrig1@uci.edu>
2023-03-08 15:46:43 -0800

    updated home page and minor css adjustments

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -1,30 +1,63 @@
 import Deadlift from "./Deadlift";
 import Rope from "./Rope";
+import Squat from "./Squat";
+import Plank from "./Plank";
+import ExerciseGIF from "./exercise_search.gif";
+import { useNavigate } from "react-router-dom";
 
 function MainPage() {
+	const navigate = useNavigate();
+
+	function handleSearch(e){
+		e.preventDefault();
+		navigate("/exercises")
+	}
+
 	return (
 		<>
 			<Deadlift />
-			<span>
-				<div className="image blck"></div>
-			</span>
-			<Rope />
-			<span>
-				<div className="image blck"></div>
+			<span className="split-container">
+				<div className="left-side half1">
+					<img src={ExerciseGIF} alt="Exercise Search GIF" autoPlay loop />
+				</div>
+				<div className="right-side half2">
+					<div>
+						<h3>Search from hundreds of exercises</h3>
+						<h5>
+							Get instant results for exercises to add to your workout plan
+						</h5>
+						<button onClick={handleSearch} className="btn search-exercise">Search Now!</button>
+					</div>
+				</div>
 			</span>
 			<Deadlift />
-			<span>
-				<div className="image blck"></div>
+			<span className="split-container">
+				<div className="left-side half2">
+					<div>Here is the left side with an image</div>
+				</div>
+				<div className="right-side half1">
+					<div>Here is the left side with inspirational text</div>
+				</div>
 			</span>
 			<Rope />
-			<span>
-				<div className="image blck"></div>
+			<span className="split-container">
+				<div className="left-side half1">
+					<div>Here is the left side with an image</div>
+				</div>
+				<div className="right-side half2">
+					<div>Here is the left side with inspirational text</div>
+				</div>
 			</span>
-			<Deadlift />
-			<span>
-				<div className="image blck"></div>
+			<Squat />
+			<span className="split-container">
+				<div className="left-side half2">
+					<div>Here is the left side with an image</div>
+				</div>
+				<div className="right-side half1">
+					<div>Here is the left side with inspirational text</div>
+				</div>
 			</span>
-			<Rope />
+			<Plank />
 		</>
 	);
 }
