98e395cc815e6c2cf99dbe9367e75e15cf77be16
Edwin <emadrig1@uci.edu>
2023-03-09 17:57:55 -0800

    merged with main

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -66,7 +66,6 @@
 // }
 // export default MainPage;
 
-
 import Deadlift from "./Deadlift";
 import Rope from "./Rope";
 import Squat from "./Squat";
@@ -74,57 +73,54 @@ import Plank from "./Plank";
 import ExerciseGIF from "./exercise_search.gif";
 import { useNavigate } from "react-router-dom";
 
-
 function MainPage() {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  function handleSearch(e) {
-    e.preventDefault();
-    navigate("/exercises");
-  }
+	function handleSearch(e) {
+		e.preventDefault();
+		navigate("/exercises");
+	}
 
-  return (
-    <div className="main-container">
-      <Deadlift />
-      <div className="centered-container">
-        <img
-          src={ExerciseGIF}
-          alt="Exercise Search GIF"
-          autoPlay
-          loop
-          className="exercise-gif"
-        />
-        <h4 className="slogan">
-          Sweat Now, Shine Later
-        </h4>
-		<h5 className="slogan">
-          Get swole and stay organized with <span>My Swole Pal™</span>
-        </h5>
-        <button onClick={handleSearch} className="search-button">
-          Search Exercises
-        </button>
-      </div>
-      <Rope />
-		<span className="split-container">
- 			<div className="left-side half2">
-				<div>Here is the left side with an image</div>
- 			</div>
- 			<div className="right-side half1">
-				<div>Here is the left side with inspirational text</div>
+	return (
+		<div className="main-container">
+			<Deadlift />
+			<div className="centered-container">
+				<img
+					src={ExerciseGIF}
+					alt="Exercise Search GIF"
+					autoPlay
+					loop
+					className="exercise-gif"
+				/>
+				<h4 className="slogan">Sweat Now, Shine Later</h4>
+				<h5 className="slogan">
+					Get swole and stay organized with <span>My Swole Pal™</span>
+				</h5>
+				<button onClick={handleSearch} className="search-button">
+					Search Exercises
+				</button>
 			</div>
- 		</span>
-      <Squat />
-	  	<span className="split-container">
- 			<div className="left-side half2">
-				<div>Here is the left side with an image</div>
- 			</div>
- 			<div className="right-side half1">
-				<div>Here is the left side with inspirational text</div>
-			</div>
- 		</span>
-      <Plank />
-    </div>
-  );
+			<Rope />
+			<span className="split-container">
+				<div className="left-side half2">
+					<div>Here is the left side with an image</div>
+				</div>
+				<div className="right-side half1">
+					<div>Here is the left side with inspirational text</div>
+				</div>
+			</span>
+			<Squat />
+			<span className="split-container">
+				<div className="left-side half2">
+					<div>Here is the left side with an image</div>
+				</div>
+				<div className="right-side half1">
+					<div>Here is the left side with inspirational text</div>
+				</div>
+			</span>
+			<Plank />
+		</div>
+	);
 }
 
 export default MainPage;
