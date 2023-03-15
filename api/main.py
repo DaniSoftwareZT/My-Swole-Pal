0d9a3f78a68b69353aeb910dcd31bdcae3b03ead
Edwin <emadrig1@uci.edu>
2023-03-15 12:30:23 -0700

    changed database url for deployment

--- api/main.py
+++ api/main.py
@@ -27,9 +27,9 @@ app.add_middleware(
 def launch_details():
     return {
         "launch_details": {
-            "year": 2022,
-            "month": 12,
-            "day": "9",
+            "year": 2023,
+            "month": 3,
+            "day": "15",
             "hour": 19,
             "min": 0,
             "tz:": "PST"
