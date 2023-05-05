4f0e499c0d0b9c5360af2e355643d299fe9e234c
Adrian <adrianchenyimo@outlook.com>
2023-05-05 01:24:05 -0500

    take out home page

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -76,11 +76,6 @@ function Nav() {
 					<div className="collapse navbar-collapse" id="navbarSupportedContent">
 						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
 							<li className="nav-item">
-								<NavLink className="nav-link" aria-current="page" to="/home">
-									Home
-								</NavLink>
-							</li>
-							<li className="nav-item">
 								<NavLink className="nav-link" to="/exercises">
 									Exercises
 								</NavLink>
